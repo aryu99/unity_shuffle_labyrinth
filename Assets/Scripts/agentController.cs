f9f4b4cf5a6d4e378048859d16205633a7b2157b
Aryaman Samyal <aryamansinghsamyal@gmail.com>
2022-09-21 17:33:50 -0500

    Agent script - Update to task descriptions

--- Assets/Scripts/agentController.cs
+++ Assets/Scripts/agentController.cs
@@ -21,6 +21,7 @@ public class agentController : Agent
     private Vector3 initLocation;
     private Vector3 targetLocation;
     private Vector3 subTaskTargetLocation;
+    public int room_ID;
 
     private Dictionary<int, (Vector3, Vector3)> taskDescriptions;
 
@@ -34,8 +35,12 @@ public class agentController : Agent
         // The key corresponds to the index of the (sub)task.
         // The value consists of two vectors. The first is the task's initial
         // location, the second is the task's target location.
+
         taskDescriptions = new Dictionary<int, (Vector3, Vector3)>();
-        taskDescriptions.Add(-1, (new Vector3(2f, 0.5f, -2f), new Vector3(1f, 0.5f, -19.5f)));
+
+        // Room #1 (TL)
+        if(room_ID == 1){
+        taskDescriptions.Add(-1, (new Vector3(1.11f, 0.5f, -1.08f), new Vector3(1f, 0.5f, -18.5f)));
         taskDescriptions.Add(0, (new Vector3(1.11f, 0.5f, -1.08f), new Vector3(5.97f, 0.5f, -3.06f)));
         taskDescriptions.Add(1, (new Vector3(1.11f, 0.5f, -1.08f), new Vector3(2.71f, 0.5f, -5.9f)));
         taskDescriptions.Add(2, (new Vector3(5.97f, 0.5f, -3.06f), new Vector3(17.33f, 0.5f, -6.06f)));
@@ -56,6 +61,83 @@ public class agentController : Agent
         taskDescriptions.Add(17, (new Vector3(17.33f, 0.5f, -13.91f), new Vector3(5.97f, 0.5f, -16.77f)));
         taskDescriptions.Add(18, (new Vector3(2.71f, 0.5f, -13.95f), new Vector3(1.0f, 0.5f, -18.5f)));
         taskDescriptions.Add(19, (new Vector3(5.97f, 0.5f, -16.77f), new Vector3(1.0f, 0.5f, -18.5f)));
+        }
+
+        // Room #2 (TR)
+        else if(room_ID == 2){
+        taskDescriptions.Add(-1, (new Vector3(1.11f + 20f, 0.5f, -1.08f), new Vector3(1f + 20f, 0.5f, -18.5f)));
+        taskDescriptions.Add(0, (new Vector3(1.11f + 20f, 0.5f, -1.08f), new Vector3(5.97f + 20f, 0.5f, -3.06f)));
+        taskDescriptions.Add(1, (new Vector3(1.11f + 20f, 0.5f, -1.08f), new Vector3(2.71f + 20f, 0.5f, -5.9f)));
+        taskDescriptions.Add(2, (new Vector3(5.97f + 20f, 0.5f, -3.06f), new Vector3(17.33f + 20f, 0.5f, -6.06f)));
+        taskDescriptions.Add(16, (new Vector3(5.97f + 20f, 0.5f, -8.12f), new Vector3(2.71f + 20f, 0.5f, -5.9f)));
+        taskDescriptions.Add(10, (new Vector3(2.71f + 20f, 0.5f, -5.9f), new Vector3(2.71f + 20f, 0.5f, -13.95f)));
+        taskDescriptions.Add(11, (new Vector3(2.71f + 20f, 0.5f, -13.95f), new Vector3(2.71f + 20f, 0.5f, -5.9f)));
+        taskDescriptions.Add(13, (new Vector3(5.97f + 20f, 0.5f, -8.12f), new Vector3(2.71f + 20f, 0.5f, -13.95f)));
+        taskDescriptions.Add(12, (new Vector3(2.71f + 20f, 0.5f, -13.95f), new Vector3(5.97f + 20f, 0.5f, -8.12f)));
+        taskDescriptions.Add(14, (new Vector3(2.71f + 20f, 0.5f, -13.95f), new Vector3(14.12f + 20f, 0.5f, -11.86f)));
+        taskDescriptions.Add(15, (new Vector3(14.12f + 20f, 0.5f, -11.86f), new Vector3(2.71f + 20f, 0.5f, -13.95f)));
+        taskDescriptions.Add(4, (new Vector3(17.33f + 20f, 0.5f, -13.91f), new Vector3(17.33f + 20f, 0.5f, -6.06f)));
+        taskDescriptions.Add(5, (new Vector3(17.33f + 20f, 0.5f, -6.06f), new Vector3(17.33f + 20f, 0.5f, -13.91f)));
+        taskDescriptions.Add(7, (new Vector3(17.33f + 20f, 0.5f, -6.06f), new Vector3(14.12f + 20f, 0.5f, -11.86f)));
+        taskDescriptions.Add(6, (new Vector3(14.12f + 20f, 0.5f, -11.86f), new Vector3(17.33f + 20f, 0.5f, -6.06f)));
+        taskDescriptions.Add(8, (new Vector3(17.33f + 20f, 0.5f, -6.06f), new Vector3(5.97f + 20f, 0.5f, -8.12f)));
+        taskDescriptions.Add(9, (new Vector3(5.97f + 20f, 0.5f, -8.12f), new Vector3(17.33f + 20f, 0.5f, -6.06f)));
+        taskDescriptions.Add(3, (new Vector3(14.12f + 20f, 0.5f, -11.86f), new Vector3(17.33f + 20f, 0.5f, -13.91f)));
+        taskDescriptions.Add(17, (new Vector3(17.33f + 20f, 0.5f, -13.91f), new Vector3(5.97f + 20f, 0.5f, -16.77f)));
+        taskDescriptions.Add(18, (new Vector3(2.71f + 20f, 0.5f, -13.95f), new Vector3(1.0f + 20f, 0.5f, -18.5f)));
+        taskDescriptions.Add(19, (new Vector3(5.97f + 20f, 0.5f, -16.77f), new Vector3(1.0f + 20f, 0.5f, -18.5f)));
+        }
+
+        // Room #3 (BL)
+        else if(room_ID == 3){
+        taskDescriptions.Add(-1, (new Vector3(1.11f, 0.5f, -1.08f - 20f), new Vector3(1f, 0.5f, -18.5f - 20f)));
+        taskDescriptions.Add(0, (new Vector3(1.11f, 0.5f, -1.08f - 20f), new Vector3(5.97f, 0.5f, -3.06f - 20f)));
+        taskDescriptions.Add(1, (new Vector3(1.11f, 0.5f, -1.08f - 20f), new Vector3(2.71f, 0.5f, -5.9f - 20f)));
+        taskDescriptions.Add(2, (new Vector3(5.97f, 0.5f, -3.06f - 20f), new Vector3(17.33f, 0.5f, -6.06f - 20f)));
+        taskDescriptions.Add(3, (new Vector3(5.97f, 0.5f, -8.12f - 20f), new Vector3(2.71f, 0.5f, -5.9f - 20f)));
+        taskDescriptions.Add(4, (new Vector3(2.71f, 0.5f, -5.9f - 20f), new Vector3(2.71f, 0.5f, -13.95f - 20f)));
+        taskDescriptions.Add(5, (new Vector3(2.71f, 0.5f, -13.95f - 20f), new Vector3(2.71f, 0.5f, -5.9f - 20f)));
+        taskDescriptions.Add(6, (new Vector3(5.97f, 0.5f, -8.12f - 20f), new Vector3(2.71f, 0.5f, -13.95f - 20f)));
+        taskDescriptions.Add(7, (new Vector3(2.71f, 0.5f, -13.95f - 20f), new Vector3(5.97f, 0.5f, -8.12f - 20f)));
+        taskDescriptions.Add(8, (new Vector3(2.71f, 0.5f, -13.95f - 20f), new Vector3(14.12f, 0.5f, -11.86f - 20f)));
+        taskDescriptions.Add(9, (new Vector3(14.12f, 0.5f, -11.86f - 20f), new Vector3(2.71f, 0.5f, -13.95f - 20f)));
+        taskDescriptions.Add(10, (new Vector3(17.33f, 0.5f, -13.91f - 20f), new Vector3(17.33f, 0.5f, -6.06f - 20f)));
+        taskDescriptions.Add(11, (new Vector3(17.33f, 0.5f, -6.06f - 20f), new Vector3(17.33f, 0.5f, -13.91f - 20f)));
+        taskDescriptions.Add(12, (new Vector3(17.33f, 0.5f, -6.06f - 20f), new Vector3(14.12f, 0.5f, -11.86f - 20f)));
+        taskDescriptions.Add(13, (new Vector3(14.12f, 0.5f, -11.86f - 20f), new Vector3(17.33f, 0.5f, -6.06f - 20f)));
+        taskDescriptions.Add(14, (new Vector3(17.33f, 0.5f, -6.06f - 20f), new Vector3(5.97f, 0.5f, -8.12f - 20f)));
+        taskDescriptions.Add(15, (new Vector3(5.97f, 0.5f, -8.12f - 20f), new Vector3(17.33f, 0.5f, -6.06f - 20f)));
+        taskDescriptions.Add(16, (new Vector3(14.12f, 0.5f, -11.86f - 20f), new Vector3(17.33f, 0.5f, -13.91f - 20f)));
+        taskDescriptions.Add(17, (new Vector3(2.71f, 0.5f, -13.95f - 20f), new Vector3(14.2f, 0.5f, -36.3f)));
+        taskDescriptions.Add(18, (new Vector3(17.3f, 0.5f, -33.5f), new Vector3(19.0f, 0.5f, -38.0f)));
+        taskDescriptions.Add(19, (new Vector3(14.2f, 0.5f, -36.3f), new Vector3(19.0f, 0.5f, -38.0f)));
+        }        
+
+        // Room #4 (BR) 
+        else if(room_ID == 4){
+        taskDescriptions.Add(-1, (new Vector3(1.11f + 20f, 0.5f, -1.08f - 20f), new Vector3(19.0f + 20f, 0.5f, -18.5f - 20f)));
+        taskDescriptions.Add(0, (new Vector3(1.11f + 20f, 0.5f, -1.08f - 20f), new Vector3(5.97f + 20f, 0.5f, -3.06f - 20f)));
+        taskDescriptions.Add(1, (new Vector3(1.11f + 20f, 0.5f, -1.08f - 20f), new Vector3(2.71f + 20f, 0.5f, -5.9f - 20f)));
+        taskDescriptions.Add(2, (new Vector3(5.97f + 20f, 0.5f, -3.06f - 20f), new Vector3(17.33f + 20f, 0.5f, -6.06f - 20f)));
+        taskDescriptions.Add(16, (new Vector3(5.97f + 20f, 0.5f, -8.12f - 20f), new Vector3(2.71f + 20f, 0.5f, -5.9f - 20f)));
+        taskDescriptions.Add(10, (new Vector3(2.71f + 20f, 0.5f, -5.9f - 20f), new Vector3(2.71f + 20f, 0.5f, -13.95f - 20f)));
+        taskDescriptions.Add(11, (new Vector3(2.71f + 20f, 0.5f, -13.95f - 20f), new Vector3(2.71f + 20f, 0.5f, -5.9f - 20f)));
+        taskDescriptions.Add(13, (new Vector3(5.97f + 20f, 0.5f, -8.12f - 20f), new Vector3(2.71f + 20f, 0.5f, -13.95f - 20f)));
+        taskDescriptions.Add(12, (new Vector3(2.71f + 20f, 0.5f, -13.95f - 20f), new Vector3(5.97f + 20f, 0.5f, -8.12f - 20f)));
+        taskDescriptions.Add(14, (new Vector3(2.71f + 20f, 0.5f, -13.95f - 20f), new Vector3(14.12f + 20f, 0.5f, -11.86f - 20f)));
+        taskDescriptions.Add(15, (new Vector3(14.12f + 20f, 0.5f, -11.86f - 20f), new Vector3(2.71f + 20f, 0.5f, -13.95f - 20f)));
+        taskDescriptions.Add(4, (new Vector3(17.33f + 20f, 0.5f, -13.91f - 20f), new Vector3(17.33f + 20f, 0.5f, -6.06f - 20f)));
+        taskDescriptions.Add(5, (new Vector3(17.33f + 20f, 0.5f, -6.06f - 20f), new Vector3(17.33f + 20f, 0.5f, -13.91f - 20f)));
+        taskDescriptions.Add(7, (new Vector3(17.33f + 20f, 0.5f, -6.06f - 20f), new Vector3(14.12f + 20f, 0.5f, -11.86f - 20f)));
+        taskDescriptions.Add(6, (new Vector3(14.12f + 20f, 0.5f, -11.86f - 20f), new Vector3(17.33f + 20f, 0.5f, -6.06f - 20f)));
+        taskDescriptions.Add(8, (new Vector3(17.33f + 20f, 0.5f, -6.06f - 20f), new Vector3(5.97f + 20f, 0.5f, -8.12f - 20f)));
+        taskDescriptions.Add(9, (new Vector3(5.97f + 20f, 0.5f, -8.12f - 20f), new Vector3(17.33f + 20f, 0.5f, -6.06f - 20f)));
+        taskDescriptions.Add(3, (new Vector3(14.12f + 20f, 0.5f, -11.86f - 20f), new Vector3(17.33f + 20f, 0.5f, -13.91f - 20f)));
+        taskDescriptions.Add(17, (new Vector3(2.71f + 20f, 0.5f, -13.95f - 20f), new Vector3(14.2f + 20f, 0.5f, -36.3f)));
+        taskDescriptions.Add(18, (new Vector3(17.3f + 20f, 0.5f, -33.5f), new Vector3(19.0f + 20f, 0.5f, -38.0f)));
+        taskDescriptions.Add(19, (new Vector3(14.2f + 20f, 0.5f, -36.3f), new Vector3(19.0f + 20f, 0.5f, -38.0f)));
+        }        
+
 
         // Set the current task to the value representing the overall task
         currentTask = -1;
@@ -111,8 +193,8 @@ public class agentController : Agent
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(transform.position);
-        sensor.AddObservation(subTaskTargetLocation);
+        sensor.AddObservation(initLocation - transform.position);
+        sensor.AddObservation(subTaskTargetLocation - initLocation);
         // sensor.AddObservation(targetTransform.position);
         sensor.AddObservation(rb.velocity.x);
         sensor.AddObservation(rb.velocity.z);
@@ -123,6 +205,10 @@ public class agentController : Agent
         float moveX = actions.ContinuousActions[0];
         float moveZ = actions.ContinuousActions[1];
         rb.AddForce(new Vector3(moveX, 0, moveZ) * moveSpeed);
+
+        // For mirrored environments
+        // rb.AddForce(new Vector3(-moveX, 0, -moveZ) * moveSpeed) // middle rooms
+        // rb.AddForce(new Vector3(-moveX, 0, moveZ) * moveSpeed)  // bottom rooms
 
         distanceToTarget = 
             Vector3.Distance(this.transform.position, targetTransform.position);
@@ -178,3 +264,5 @@ public class agentController : Agent
     }
 
 }
+
+
